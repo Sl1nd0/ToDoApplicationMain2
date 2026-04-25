09ef81a018e9c55a866146ae9af287a8b2494cb0
Sl1nd0 <ssankabi@gmail.com>
2026-04-25 16:22:47 +0200

    Add seed

---
+++ Messages.Seed/Program.cs
@@ -0,0 +1,29 @@
+﻿using Messages.Seed;
+using Messages.Shared.DTO.Requests.CommandRequests;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Core.Interfaces;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var svc = new ServiceCollection();
+        RegisterServices.Register(svc);
+        var services = svc.BuildServiceProvider();
+
+        var addTask = services.GetRequiredService<ICommandHandler<AddUserRoleRequest>>();
+
+        var addUserRoleRequest = new AddUserRoleRequest
+        {
+            RoleName = "Admin",
+        };
+
+        var addResult = addTask.Execute(addUserRoleRequest);
+        addResult.Wait();
+
+        var result = addResult.Result;
+
+        Console.WriteLine("Hello, World!");
+    }
+
+}
